07c73104e278f6b94e69c173419f8d804444fdab
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-01-22 17:33:07 +0000

    Update fitms_nf.R

--- bin/fitms_nf.R
+++ bin/fitms_nf.R
@@ -2,5 +2,19 @@
 
 import pandas as pd
 from dataclasses import dataclass
+
+@dataclass
+class SequenceRange:
+    """Class for the start and end of a range."""
+    name: str
+    start: int
+    end: int
+    chrom: str
+    def overlaps(self, other: "SequenceRange") -> bool:
+        if self.chrom != other.chrom:
+            return False
+        return (other.start <= self.start <= other.end) or (other.start <= self.end <= other.end) or (self.start <= other.start <= self.end) or (self.start <= other.end <= self.end)
+        
+        
 with open('readme.txt', 'w') as f:
     f.write('readme')

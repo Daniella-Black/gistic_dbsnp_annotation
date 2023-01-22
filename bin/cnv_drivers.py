46d37bfe9ff5412ba941e40db80935dda755935c
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-01-22 22:55:05 +0000

    Update cnv_drivers.py

--- bin/cnv_drivers.py
+++ bin/cnv_drivers.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from dataclasses import dataclass
 import argparse
+#from os.path import exists
 
 @dataclass
 class SequenceRange:
@@ -49,27 +50,34 @@ if ploidy <2.5:
 elif ploidy >= 2.5:
     amp_threshold = 9
 
-cnv = pd.read_csv(cnv_path, '\t')
-cnv['total_cn'] = cnv['major_cn'] + cnv['minor_cn']
-total_cn = list(cnv['total_cn'])
-cnv['id'] = cnv['seqnames'].astype(str) + '_' + cnv['start'].astype(str) + '_' + cnv['end'].astype(str) + '_' + cnv['total_cn'].astype(str) +'_' + sample
-id_list = list(cnv['id'])
-for contig in range(len(total_cn)):
-    if total_cn[contig] >= amp_threshold: 
-        amps.append(id_list[contig]) 
-##for each contig (no matter if it is amplified) report whether any contig overlaps with gene of interest - this is to identify samples with no data for the gene of interest for i in range(len(gene_df.index)): ##need file with all coding gene name chromosome coordinates #for gene in gene_df:
-for i in range(len(gene_df.index)):  
-    gene  = SequenceRange(gene_df['gene_name'][i], gene_df['start'][i], gene_df['end'][i], gene_df['chr'][i])
-    contig_overlapping_gene = list()
-    cnv_chr = cnv.loc[cnv['seqnames'].astype('str') == gene.chrom]
-    cnv_chr.index = pd.RangeIndex(len(cnv_chr.index))
-    for contig in range(len(list(cnv_chr['total_cn']))):
-        contig_range= SequenceRange('place_holder', int(cnv_chr['start'][contig]), int(cnv_chr['end'][contig]), str(cnv_chr['seqnames'][contig]))
-        if contig_range.overlaps(gene):
-            contig_overlapping_gene.append(id_list[contig])
-    if len(contig_overlapping_gene)== 0:
-        missing_gene_data_sample.append(gene.name + '_' + sample)
-#print(sample)
+    
+#file_exists = exists(cnv_path):
+try:    
+    cnv = pd.read_csv(cnv_path, '\t')
+    cnv['total_cn'] = cnv['major_cn'] + cnv['minor_cn']
+    total_cn = list(cnv['total_cn'])
+    cnv['id'] = cnv['seqnames'].astype(str) + '_' + cnv['start'].astype(str) + '_' + cnv['end'].astype(str) + '_' + cnv['total_cn'].astype(str) +'_' + sample
+    id_list = list(cnv['id'])
+    for contig in range(len(total_cn)):
+        if total_cn[contig] >= amp_threshold: 
+            amps.append(id_list[contig]) 
+    ##for each contig (no matter if it is amplified) report whether any contig overlaps with gene of interest - this is to identify samples with no data for the gene of interest for i in range(len(gene_df.index)): ##need file with all coding gene name chromosome coordinates #for gene in gene_df:
+    for i in range(len(gene_df.index)):  
+        gene  = SequenceRange(gene_df['gene_name'][i], gene_df['start'][i], gene_df['end'][i], gene_df['chr'][i])
+        contig_overlapping_gene = list()
+        cnv_chr = cnv.loc[cnv['seqnames'].astype('str') == gene.chrom]
+        cnv_chr.index = pd.RangeIndex(len(cnv_chr.index))
+        for contig in range(len(list(cnv_chr['total_cn']))):
+            contig_range= SequenceRange('place_holder', int(cnv_chr['start'][contig]), int(cnv_chr['end'][contig]), str(cnv_chr['seqnames'][contig]))
+            if contig_range.overlaps(gene):
+                contig_overlapping_gene.append(id_list[contig])
+        if len(contig_overlapping_gene)== 0:
+            missing_gene_data_sample.append(gene.name + '_' + sample)
+    #print(sample)
+except FileNotFoundError as e:
+    with open(sample + 'mtr_format_cnv_missing.txt', 'w') as f:
+        f.write(sample+' no mtr format cnv file')
+        
 if len(missing_gene_data_sample) >0:
     missing_data_samples_gene_df = pd.DataFrame(missing_gene_data_sample)
     #missing_data_samples_gene_df = missing_data_samples_mdm2_df.rename (columns={0: 'missing_mdm2_samples'}) missing_data_samples_gene_df [['sample', 'gene']] = missing_data_samples_gene_df[0].str.split(', 1, expand=True)

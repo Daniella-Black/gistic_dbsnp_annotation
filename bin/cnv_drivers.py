aa318f2f61c5bce889d79eb6a9fe930477c4aef8
Daniella-Black <93602609+Daniella-Black@users.noreply.github.com>
2023-01-28 14:01:29 +0000

    Update cnv_drivers.py

--- bin/cnv_drivers.py
+++ bin/cnv_drivers.py
@@ -82,13 +82,13 @@ try:
             if contig_range.overlaps(gene):
                 contig_overlapping_gene.append(id_list[contig])
         if len(contig_overlapping_gene)== 0:
-            missing_gene_data_sample.append(gene.name + '_' + sample +'_' + gene.transcript + '_' +str(gene.start) + '_' +str(gene.end) + '_' +gene.chr)
+            missing_gene_data_sample.append(gene.name + '_' + sample +'_' + gene.transcript + '_' +str(gene.start) + '_' +str(gene.end) + '_' +gene.chrom)
         ##report the genes in each amp
         if len(amps) > 0:    
             for amp in range(len(amps_df.index)):
                 amp_range= SequenceRange('place_holder', 'place_holder', int(amps_df['start'][amp]), int(amps_df['end'][amp]), str(amps_df['chr'][amp]))
                 if amp_range.overlaps(gene) and amp_range.chrom == gene.chrom:
-                    genes_in_amps[amp].append(gene.name + '_' + sample +'_' + gene.transcript + '_' +str(gene.start) + '_' +str(gene.end) + '_' +gene.chr)
+                    genes_in_amps[amp].append(gene.name + '_' + sample +'_' + gene.transcript + '_' +str(gene.start) + '_' +str(gene.end) + '_' +gene.chrom)
     amps_df['genes_in_amps'] = genes_in_amps
         
     #print(sample)
